ac61dfaaba06c609d0d9579df7b460ab0b8a0921
mayfreq <osmankalkan001@gmail.com>
2021-08-14 18:14:55 +0300

    [photo-gallery] initial commit

---
+++ photo-gallery/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h3>photo gallery with search</h3>
+    </React.Fragment>
+  );
+};
+
+export default App;
