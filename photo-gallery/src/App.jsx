9b1ab98f4fb86017bb2d775562c6ec9a40307729
mayfreq <osmankalkan001@gmail.com>
2021-08-18 18:28:56 +0300

    [photo-gallery] added basic design with sample data

--- photo-gallery/src/App.jsx
+++ photo-gallery/src/App.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+
   return (
     <React.Fragment>
-      <h3>photo gallery with search</h3>
+      <div className="form">
+        <input type="text" name="" placeholder="Search" />
+      </div>
+      <div className="list">
+        <ul>
+          <li><img src="https://picsum.photos/id/240/1920/1080" alt="" /></li>
+          <li><img src="https://picsum.photos/id/241/900/600" alt="" /></li>
+          <li><img src="https://picsum.photos/id/242/1920/1080" alt="" /></li>
+          <li><img src="https://picsum.photos/id/243/1024/768" alt="" /></li>
+          <li><img src="https://picsum.photos/id/244/1400/768" alt="" /></li>
+          <li><img src="https://picsum.photos/id/250/1200/600" alt="" /></li>
+          <li><img src="https://picsum.photos/id/251/1920/1080" alt="" /></li>
+          <li><img src="https://picsum.photos/id/247/1024/900" alt="" /></li>
+          <li><img src="https://picsum.photos/id/248/1354/1024" alt="" /></li>
+          <li><img src="https://picsum.photos/id/249/1920/1080" alt="" /></li>
+          <li></li>
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
